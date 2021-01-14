803f3b3c84d9746be328782e4cb8b14de85c6562
Mngatia <nyawirahngatia@gmail.com>
2021-01-14 18:08:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/retrofitppp/PostRequestApi.java
@@ -0,0 +1,12 @@
+package com.example.retrofitppp;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface PostRequestApi {
+    // https://httpbin.org/post
+
+    @POST("post")
+    Call<PostModel>PostDataIntoServer(@Body PostModel postModel);
+}
